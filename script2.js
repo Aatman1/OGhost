fb0d7e793c7312ed8b2332a4c118c9eb904210ee
Aatman1 <aatmans29@gmail.com>
2024-06-04 07:06:39 -0500

    fix

--- script2.js
+++ script2.js
@@ -220,4 +220,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 });
+<<<<<<< HEAD
 >>>>>>> parent of eb81958 (MANY FLAG)
+=======
+            
+$(".dropdown dd ul li a").click(function() {
+    var text = $(this).html();
+    $(".dropdown dt a span").html(text);
+    $(".dropdown dd ul").hide();
+    $("#result").html("Selected value is: " + getSelectedValue("sample"));
+});
+            
+function getSelectedValue(id) {
+    return $("#" + id).find("dt a span.value").html();
+}
+
+$(document).bind('click', function(e) {
+    var $clicked = $(e.target);
+    if (! $clicked.parents().hasClass("dropdown"))
+        $(".dropdown dd ul").hide();
+});
+
+
+
+    $(".dropdown img.flag").toggleClass("flagvisibility");
+
+>>>>>>> parent of fd80a4a (Update script2.js)
