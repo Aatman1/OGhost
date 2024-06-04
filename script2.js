52b3e2b1993378d8f8ba386650f37f75c1722536
Aatman1 <aatmans29@gmail.com>
2024-06-04 06:04:44 -0500

    correct

--- script2.js
+++ script2.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-$(".dropdown img.flag").addClass("flagvisibility");
-
-$(".dropdown dt a").click(function() {
-    $(".dropdown dd ul").toggle();
-});
-            
-$(".dropdown dd ul li a").click(function() {
-    var text = $(this).html();
-    $(".dropdown dt a span").html(text);
-    $(".dropdown dd ul").hide();
-    $("#result").html("Selected value is: " + getSelectedValue("sample"));
-});
-            
-function getSelectedValue(id) {
-    return $("#" + id).find("dt a span.value").html();
-}
-
-$(document).bind('click', function(e) {
-    var $clicked = $(e.target);
-    if (! $clicked.parents().hasClass("dropdown"))
-        $(".dropdown dd ul").hide();
-});
-
-
-
-    $(".dropdown img.flag").toggleClass("flagvisibility");
-  countries.forEach((country, index) => {
-      const option = document.createElement('option');
-      option.value = country.code;
-      option.text = country.label;
-      select.appendChild(option);
-=======
 document.addEventListener('DOMContentLoaded', function () {
     // DOM Element References
     const signInForm = document.getElementById('signInForm');
